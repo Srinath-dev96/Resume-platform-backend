efd52a29dd1a80969dd1164de7d7ba4af2f4cc40
Srinath V <varikuppulashreenath@gmail.com>
2025-09-16 17:39:32 +0530

    Update: MongoDB Atlas connection

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error(err.message);
+    await mongoose.connect("mongodb+srv://Srinath:<Shree1697>@cluster0.34gkwxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("MongoDB connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Failed",error);
     process.exit(1);
   }
 };
